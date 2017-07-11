e21118bf9a811b901c0f3aa02c51d964d0a1ab29
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-11 19:06:55 -0400

    Finished Storyboard, and basic gameplay.

--- MyTapperGame-IOS/ViewController.swift
+++ MyTapperGame-IOS/ViewController.swift
@@ -10,17 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var score = 0;
+    var time = 0;
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World! Again")
+        setup()
+        //Once the game starts, the timer is initialized
+        [Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timer)  , userInfo: nil, repeats: true)]
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setup(){
+        score = 0;
+        time = 60
+    }
 
+    @IBAction func tapButtonClicked(_ sender: Any) {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+        
+        
+    }
+    
+    func timer() {
+        if time != 0{
+         time -= 1
+            timeLabel.text = "Time: \(time)"
+        } else {
+            //Segue to GameOver View
+        }
+        
+    }
+    func moveToView(){
+        
+    }
+    
 
 }
 
