00057144dab3de333ae6b6bfef07b8ebb12fc8b6
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-11 16:40:00 -0400

    Initial Commit

---
+++ MyTapperGame-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTapperGame-IOS
+//
+//  Created by Daniel on 7/11/17.
+//  Copyright © 2017 Daniel Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
