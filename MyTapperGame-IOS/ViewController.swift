47bacbe63be059fd1e1b7cac101fae899862cb07
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-12 00:10:31 -0400

    Finished VC and tap2start feature!

--- MyTapperGame-IOS/ViewController.swift
+++ MyTapperGame-IOS/ViewController.swift
@@ -8,22 +8,38 @@
 
 import UIKit
 
+var score = 0;
+var time = 0;
+
 class ViewController: UIViewController {
 
-    var score = 0;
-    var time = 0;
+    
+    @IBOutlet weak var tapToStart: UIButton!
     
     @IBOutlet weak var scoreLabel: UILabel!
     
     @IBOutlet weak var timeLabel: UILabel!
     
+    enum gameState {
+        case beforeGame;
+        case duringGame;
+        case afterGame;
+    }
+    
+    var timer: Timer?;
+    
+    var currentGameState = gameState.beforeGame
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setup()
+        currentGameState = gameState.beforeGame
         //Once the game starts, the timer is initialized
-        [Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timer)  , userInfo: nil, repeats: true)]
+        
+        
+        
+        
     }
     
 
@@ -34,26 +50,50 @@ class ViewController: UIViewController {
     
     func setup(){
         score = 0;
-        time = 60
+        time = 15;
+        tapToStart.isEnabled = true;
+        tapToStart.isUserInteractionEnabled = true;
+        tapToStart.isHidden = false;
+        
     }
 
     @IBAction func tapButtonClicked(_ sender: Any) {
+        if(time != 0 && currentGameState == gameState.duringGame) {
         score += 1
         scoreLabel.text = "Score: \(score)"
-        
+        }
         
     }
     
-    func timer() {
+    func countDownTimer() {
         if time != 0{
          time -= 1
             timeLabel.text = "Time: \(time)"
         } else {
             //Segue to GameOver View
-        }
+            timer?.invalidate()
+            timer = nil
+            moveToView()
+                    }
         
     }
+    
+    @IBAction func tapToStartTapped(_ sender: Any) {
+        tapToStart.isEnabled = false;
+        tapToStart.isUserInteractionEnabled = false
+        tapToStart.isHidden = true
+        currentGameState = .duringGame
+         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.countDownTimer)  , userInfo: nil, repeats: true)
+        timeLabel.text = "Time: \(time)"
+    }
+    
+    
+    
     func moveToView(){
+        
+       self.performSegue(withIdentifier: "segue", sender: nil)
+ 
+        
         
     }
     
